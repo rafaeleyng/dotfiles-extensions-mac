0c7a09fe61be17a7cdcadd48a6a0fdb60b848c9c
rafaeleyng <rafaeleyng@gmail.com>
2020-10-17 17:40:13 -0300

    adjuste output sound device name

--- shell/index.zsh
+++ shell/index.zsh
@@ -9,11 +9,11 @@ S_OUT_RAW="MacBook Pro Speakers"
 S_INP_SIMPLE="External Microphone"
 S_OUT_SIMPLE="External Headphones"
 
-S_INP_FANCY="Microsoft LifeChat LX-3000"
+S_INP_FANCY="JBL LIVE650BTNC"
 S_OUT_FANCY=$S_INP_FANCY
 
 S_INP_PRO="ARCANO AM-BLACK-1 " # yes, with a trailing space ¯\_(ツ)_/¯
-S_OUT_PRO="Microsoft LifeChat LX-3000"
+S_OUT_PRO="JBL LIVE650BTNC"
 
 # BlackHole, see:
 # - https://github.com/ExistentialAudio/BlackHole
